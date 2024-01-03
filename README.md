edca24611f89a0a4f9290379da560b726f300775
Philippe Ozil <pozil@salesforce.com>
2024-01-03 12:29:38 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Stream Deck Restream Server
+
+## Overview
+
+Server for the unofficial Elgato Stream Deck plugin that controls Restream Studio via a Chrome Extension.
+
+This solution requires three components to run:
+1. A Node.js server (this project)
+1. A Chrome extension
+1. A [Stream Deck plugin](https://github.com/pozil/streamdeck-restream-plugin)
+
+## Server Setup
+
+1. Ensure that you have [Node.js](https://nodejs.org/en) v20 or above by running this command in a terminal:
+    ```sh
+    node -v
+    ```
+
+1. Install the server dependencies:
+    ```sh
+    npm install
+    ```
+
+1. Start the server:
+    ```sh
+    npm start
+    ```
+
+Now that the server is up and running, install the Chrome extension.
---
+++ src/server.js
@@ -0,0 +1,61 @@
+import { WebSocketServer } from 'ws';
+import url from 'node:url';
+
+const CLIENT_BROWSER = 'browser';
+const CLIENT_STREAMDECK = 'streamdeck';
+
+console.log('Starting WSS on port 5000');
+const wss = new WebSocketServer({ port: 5000 });
+
+let browserClient, sdClient;
+
+function handleMessages(sourceClientType, data) {
+  const messageJson = data.toString();
+  console.log('WS received from %s: %s', sourceClientType, messageJson);
+  if (sourceClientType === CLIENT_STREAMDECK) {
+    if (browserClient) {
+      browserClient.send(messageJson);
+    } else {
+      console.error(`Cannot send action: Browser WS not available`);
+      const originalData = JSON.parse(messageJson);
+      sdClient.send(JSON.stringify({ type: 'not-delivered', data: originalData }));
+    }
+  } else if (sourceClientType === CLIENT_BROWSER) {
+    if (sdClient) {
+      sdClient.send(messageJson);
+    } else {
+      console.error(`Cannot send action: Stream Deck WS not available`);
+      const originalData = JSON.parse(messageJson);
+      browserClient.send(JSON.stringify({ action: 'not-delivered', data: originalData }));
+    }
+  }
+}
+
+wss.on('connection', (ws, req) => {
+  const reqParams = url.parse(req.url, true);
+  const clientType = reqParams.query['client-type'];
+  console.log(`WS client connected: ${clientType}`);
+  
+  // Save client references
+  if (clientType === CLIENT_BROWSER) {
+    browserClient = ws;
+  } else if (clientType === CLIENT_STREAMDECK) {
+    sdClient = ws;
+    browserClient?.send(JSON.stringify({ action: 'sd-connected' }));
+  } else {
+    console.error(`Unsupported client type: ${clientType}`);
+  }
+
+  ws.on('message', (data) => {
+    handleMessages(clientType, data);
+  });
+
+  ws.on('close', () => {
+    console.log(`WS client disconnected: ${clientType}`);
+    if (clientType === CLIENT_BROWSER) {
+      browserClient = undefined;
+    } else if (clientType === CLIENT_STREAMDECK) {
+      sdClient = undefined;
+    }
+  });
+});
